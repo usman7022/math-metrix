002608537339a9ebcf05e6fb336f09a2c8667082
usman7022 <usmanmunir7022@gmail.com>
2024-06-05 14:51:36 -0700

    final commit

---
+++ math-metrix/android/app/src/main/kotlin/com/nividata/mathmatrix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nividata.mathmatrix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ math-metrix/lib/src/data/models/game_category.dart
@@ -0,0 +1,22 @@
+import 'package:mathgame/src/core/app_constant.dart';
+import 'package:mathgame/src/data/models/score_board.dart';
+
+class GameCategory {
+  int id;
+  String name;
+  String key;
+  GameCategoryType gameCategoryType;
+  String routePath;
+  ScoreBoard scoreboard;
+  String icon;
+
+  GameCategory(
+    this.id,
+    this.name,
+    this.key,
+    this.gameCategoryType,
+    this.routePath,
+    this.scoreboard,
+    this.icon,
+  );
+}
